416db2670f9a084937043bddb5115ab11d726995
Brian Heim <bheim67@gmail.com>
2016-09-03 20:11:44 -0600

    finished i3

---
+++ spec/features/user_creates_a_song_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "user submits a new song" do
+  scenario "they can see the page for an indivicual song" do
+    artist = create(:artist)
+
+    song_title = "Iron Lion Zion"
+
+    visit artist_path(artist)
+    click_on "New Song"
+    fill_in "song_title", with: song_title
+    click_on "Create Song"
+
+    expect(page).to have_content song_title
+    expect(page).to have_link artist.name, href: artist_path(artist)
+  end
+end
