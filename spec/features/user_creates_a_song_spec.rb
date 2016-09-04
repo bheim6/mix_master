1085aa0732d4c673bf8e4de7be017320ff4222a1
Brian Heim <bheim67@gmail.com>
2016-09-03 20:47:42 -0600

    added song feature title cannot be blank

--- spec/features/user_creates_a_song_spec.rb
+++ spec/features/user_creates_a_song_spec.rb
@@ -14,4 +14,16 @@ RSpec.feature "user submits a new song" do
     expect(page).to have_content song_title
     expect(page).to have_link artist.name, href: artist_path(artist)
   end
+
+  context "the data submitted is invalid" do
+    scenario "they cannot leave song title blank" do
+      artist = create(:artist)
+
+      visit artist_path(artist)
+      click_on "New Song"
+      click_on "Create Song"
+
+      expect(page).to have_content "Title can't be blank"
+    end
+  end
 end
